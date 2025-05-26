161c3974db4be154e42f5103da073f11783852f1
DeadFiender <artem.bibekin@gmail.com>
2025-05-26 13:28:04 +0700

    New connect

--- window.cpp
+++ window.cpp
@@ -10,5 +10,5 @@ btn = new QPushButton("Завершить",this );
 QVBoxLayout *layout = new QVBoxLayout(this);
 layout->addWidget(area);
 layout->addWidget(btn);
-connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+connect(btn, &QPushButton::clicked,this,&QWidget::close);
 };
